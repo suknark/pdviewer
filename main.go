d891b7de3a25c9eb10e4ac6a6e8a8df3ca64623a
Roman Krankus <rkrankus@fun.co>
2019-12-17 11:47:46 +0300

    Tuning  envs

--- main.go
+++ main.go
@@ -8,12 +8,17 @@ import (
 
 func main() {
 	token := os.Getenv("PDTOKEN")
-	schedules := os.Getenv("SHEDULES")
+	schedules := os.Getenv("PDSHEDULES")
 	pd := NewPdApi(token, schedules)
 	fs := http.FileServer(http.Dir("./public/static"))
 	handlers := NewHandlers(pd)
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.Handle("/", handlers)
-	log.Println("Listening...")
-	http.ListenAndServe(os.Getenv("HOST"), nil)
+	if os.Getenv("PDVIEWERLISTEN") == "" {
+		log.Println("PDVIEWERLISTEN not set. Using  0.0.0.0:9090")
+		http.ListenAndServe(":9090", nil)
+	} else {
+		log.Println("Listening on "+os.Getenv("PDVIEWERLISTEN"))
+		http.ListenAndServe(os.Getenv("PDVIEWERLISTEN"), nil)
+	}
 }
